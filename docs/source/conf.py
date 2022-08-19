205adc553cec875c83f5714b204c6da211ced00c
stupid-cloud <clqin@foxmail.com>
2022-08-19 13:30:31 +0800

    2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,37 +1,31 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Pymatsci'
-copyright = '2022, Pymatsci'
+copyright = '2022, C.L. Qin'
 author = 'C.L. Qin'
-
 release = '1.0.0'
-version = '1.0.0'
 
-# -- General configuration
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
     'recommonmark',
     'sphinx_markdown_tables'
-]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+    ]
 
 templates_path = ['_templates']
+exclude_patterns = []
 
-# -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
